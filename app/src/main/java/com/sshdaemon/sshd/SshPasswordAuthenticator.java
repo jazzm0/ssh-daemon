761bd109207dccd1877c1c53c1c465fe1b957397
jazzm0 <tibor.tarnai@gmail.com>
2023-03-24 17:00:41 +0100

    Feature/add read only mode (#42)

--- app/src/main/java/com/sshdaemon/sshd/SshPasswordAuthenticator.java
+++ app/src/main/java/com/sshdaemon/sshd/SshPasswordAuthenticator.java
@@ -7,6 +7,8 @@ import org.apache.sshd.server.auth.password.PasswordAuthenticator;
 import org.apache.sshd.server.auth.password.PasswordChangeRequiredException;
 import org.apache.sshd.server.session.ServerSession;
 
+import java.util.Objects;
+
 public class SshPasswordAuthenticator implements PasswordAuthenticator {
 
     private final String user;
@@ -20,5 +22,18 @@ public class SshPasswordAuthenticator implements PasswordAuthenticator {
     @Override
     public boolean authenticate(String username, String password, ServerSession session) throws PasswordChangeRequiredException, AsyncAuthException {
         return username.equals(user) && password.equals(this.password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SshPasswordAuthenticator that = (SshPasswordAuthenticator) o;
+        return Objects.equals(user, that.user) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, password);
     }
 }
--- app/src/test/java/com/sshdaemon/sshd/SshDaemonTest.java
+++ app/src/test/java/com/sshdaemon/sshd/SshDaemonTest.java
@@ -5,11 +5,53 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import android.os.Environment;
+
+import org.apache.sshd.contrib.server.subsystem.sftp.SimpleAccessControlSftpEventListener;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.subsystem.SubsystemFactory;
+import org.apache.sshd.sftp.server.SftpSubsystemFactory;
 import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.IOException;
+import java.util.List;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Environment.class, SshServer.class})
 public class SshDaemonTest {
+
+    @Rule
+    TemporaryFolder tempFolder = new TemporaryFolder();
+
+    SshServer sshServer = mock(SshServer.class);
+
+    @Captor
+    ArgumentCaptor<List<? extends SubsystemFactory>> captor;
+
+    @Before
+    public void doSetup() throws IOException {
+        PowerMockito.mockStatic(Environment.class);
+        PowerMockito.mockStatic(SshServer.class);
+
+        when(Environment.getExternalStorageDirectory()).thenReturn(tempFolder.newFolder());
+        when(SshServer.setUpDefaultServer()).thenReturn(sshServer);
+    }
+
 
     @Test
     public void testLoadKeys() {
@@ -18,5 +60,30 @@ public class SshDaemonTest {
         assertThat(fingerPrints.containsKey(SshFingerprint.DIGESTS.SHA256), Matchers.is(true));
         assertThat(fingerPrints.get(SshFingerprint.DIGESTS.MD5), is(not(nullValue())));
         assertThat(fingerPrints.get(SshFingerprint.DIGESTS.SHA256), is(not(nullValue())));
+    }
+
+    @Test
+    public void testReadOnlySubsystem() {
+        new SshDaemon(22, "user", "password", true);
+
+        verify(sshServer).setPort(22);
+        verify(sshServer).setPasswordAuthenticator(new SshPasswordAuthenticator("user", "password"));
+        verify(sshServer).setSubsystemFactories(captor.capture());
+        var factories = captor.getValue();
+        assertThat(factories.size(), is(1));
+        assertThat(((SftpSubsystemFactory) factories.get(0)).getRegisteredListeners().size(), is(1));
+        assertTrue(((SftpSubsystemFactory) factories.get(0)).getRegisteredListeners().contains(SimpleAccessControlSftpEventListener.READ_ONLY_ACCESSOR));
+    }
+
+    @Test
+    public void testReadWriteSubsystem() {
+        new SshDaemon(22, "user", "password", false);
+
+        verify(sshServer).setPort(22);
+        verify(sshServer).setPasswordAuthenticator(new SshPasswordAuthenticator("user", "password"));
+        verify(sshServer).setSubsystemFactories(captor.capture());
+        var factories = captor.getValue();
+        assertThat(factories.size(), is(1));
+        assertThat(((SftpSubsystemFactory) factories.get(0)).getRegisteredListeners().size(), is(0));
     }
 }
