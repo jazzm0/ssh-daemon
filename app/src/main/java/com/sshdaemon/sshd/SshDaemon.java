641cc1cf5ff73016384d53a6c40f7a015ae1ec93
d050150 <tibor.tarnai@sap.com>
2025-03-28 14:37:52 +0100

    Remove compression factories

--- app/src/main/java/com/sshdaemon/sshd/SshDaemon.java
+++ app/src/main/java/com/sshdaemon/sshd/SshDaemon.java
@@ -11,9 +11,6 @@ import static org.apache.sshd.common.cipher.BuiltinCiphers.aes128gcm;
 import static org.apache.sshd.common.cipher.BuiltinCiphers.aes192ctr;
 import static org.apache.sshd.common.cipher.BuiltinCiphers.aes256ctr;
 import static org.apache.sshd.common.cipher.BuiltinCiphers.aes256gcm;
-import static org.apache.sshd.common.compression.BuiltinCompressions.delayedZlib;
-import static org.apache.sshd.common.compression.BuiltinCompressions.none;
-import static org.apache.sshd.common.compression.BuiltinCompressions.zlib;
 import static java.lang.Math.max;
 import static java.util.Objects.requireNonNull;
 
@@ -136,7 +133,6 @@ public class SshDaemon extends Service {
         this.sshd = ServerBuilder
                 .builder()
                 .cipherFactories(List.of(aes128ctr, aes192ctr, aes256ctr, aes128gcm, aes256gcm))
-                .compressionFactories(List.of(none, zlib, delayedZlib))
                 .build();
 
         sshd.setPort(port);
